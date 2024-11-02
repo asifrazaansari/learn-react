7ffc635fde13e88299ddd8efb5d86a023971f2fa
asifrazaansari <asifraza0231@gmail.com>
2024-11-02 08:01:53 +0530

    React Basic

---
+++ react-01-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Learn from "./Learn"
+
+function App() {
+  return (
+    <>
+      <Learn />
+      <h1>Learn React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ react-01-project/src/Learn.jsx
@@ -0,0 +1,7 @@
+function Learn() {
+    return (
+        <h3>Learn with me</h3>
+    )
+}
+
+export default Learn
